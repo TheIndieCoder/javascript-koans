a0732965196132b9714b5f72e4ecb8e5fb3712d7
tutp <paul.nabarun@gmail.com>
2017-12-12 22:57:05 +0530

    Revert "Fixed wrongly placed asserts in AboutExpects.js"
    This reverts commit 110404bc0eea06cb1ad470fdc134aef5e495900e.

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -4,15 +4,15 @@ describe('About Expects', function() {
   it('should expect true', function() {
 
     // Your journey begins here: Replace the word false with true 
-    expect(true).toBeTruthy();
+    expect(false).toBeTruthy();
   });
 
   // To understand reality, we must compare our expectations against reality.
   it('should expect equality', function() {
-    var expectedValue = FILL_ME_IN;;
+    var expectedValue = FILL_ME_IN;
     var actualValue = 1 + 1;
 
-    expect(actualValue !== expectedValue).toBeTruthy();
+    expect(actualValue === expectedValue).toBeTruthy();
   });
 
   // Some ways of asserting equality are better than others.
@@ -21,7 +21,7 @@ describe('About Expects', function() {
     var actualValue = 1 + 1;
 
   // toEqual() compares using common sense equality.
-    expect(actualValue).toNotEqual(expectedValue);
+    expect(actualValue).toEqual(expectedValue);
   });
 
   // Sometimes you need to be precise about what you "type."
@@ -30,11 +30,11 @@ describe('About Expects', function() {
     var actualValue = (1 + 1).toString();
 
   // toBe() will always use === to compare.
-    expect(actualValue).toNotBe(expectedValue);
+    expect(actualValue).toBe(expectedValue);
   });
 
   // Sometimes we will ask you to fill in the values.
   it('should have filled in values', function() {
-    expect('Fill this value in').toEqual(FILL_ME_IN);
+    expect(1 + 1).toEqual(FILL_ME_IN);
   });
 });
